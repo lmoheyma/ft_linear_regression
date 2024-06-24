9f3f598af67ab17da009edadbac1df2fce0ebdb1
lmoheyma <louismoheymani@gmail.com>
2024-06-24 23:48:07 +0200

    ✅ Plotting

--- leastSquareEstimation.py
+++ leastSquareEstimation.py
@@ -1,5 +1,10 @@
-import numpy as np
+from matplotlib import pyplot as plt
 import pandas as pd
+
+def displayLinearRegression(x: float, y: float, theta0: float, theta1: float) -> int:
+	plt.scatter(x, y)
+	linearRegression = theta0 + (theta1 * x)
+	plt.plot(x, linearRegression, color='red')
 
 def predictPrice(data: pd.core.frame.DataFrame) -> int:
 	x = data['km']
@@ -7,13 +12,11 @@ def predictPrice(data: pd.core.frame.DataFrame) -> int:
 	n = len(x)
 	mean_x = sum(x) / n
 	mean_y = sum(y) / n
-	print(mean_x, mean_y)
 
-	SS_xy = sum([(y[ind] * x[ind]) - (n * (mean_x) * (mean_y)) for ind in x.index])
-	[print(x[ind], y[ind]) for ind in x.index]
-	SS_xx = sum([(x[ind] * x[ind]) - n * ((mean_x) * (mean_x)) for ind in x.index])
+	SS_xy = sum([(y[ind] * x[ind]) for ind in x.index]) - (n * (mean_x) * (mean_y))
+	SS_xx = sum([(x[ind] * x[ind]) for ind in x.index]) - n * ((mean_x) * (mean_x)) 
 
 	theta_1 = (SS_xy / SS_xx)
 	theta_0 = (mean_y - (theta_1 * mean_x))
-	
+	displayLinearRegression(x, y, theta_0, theta_1)
 	return (theta_0, theta_1)
--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 from leastSquareEstimation import predictPrice
 from load_csv import load
-import pandas as pd
+from matplotlib import pyplot as plt
 
 def main():
 	data = load("data.csv")
-	theta = predictPrice(data)
 	mileage = input("Mileage: ")
 	try:
 		mileage = float(mileage)
 	except ValueError:
 		print("Type Error")
 		exit(1)
+	theta = predictPrice(data)
 	estimatePrice = theta[0] + (theta[1] * mileage)
 	print(f"estimatePrice({mileage}) = {theta[0]:2f} + ({theta[1]:2f} ∗ {mileage}) = {estimatePrice:2f}")
+	plt.show()
 	return 0
 
 if __name__ == "__main__":
