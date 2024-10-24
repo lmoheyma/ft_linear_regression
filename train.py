dc5d2e610344c5ec8dc52e321d13a98aa743037e
lmoheyma <louismoheymani@gmail.com>
2024-10-24 22:49:13 +0200

    ✅ Denormalized thetas

--- train.py
+++ train.py
@@ -5,6 +5,7 @@ import time
 
 class LinearRegression:
 	def __init__(self) -> None:
+		self.mileage = 0
 		self.theta0 = 0.0
 		self.theta1 = 0.0
 		self.data = load("data.csv")
@@ -20,14 +21,13 @@ class LinearRegression:
 		return pd.DataFrame({'km': normalizeSerie(self.dataMileages), 'price': normalizeSerie(self.dataPrices)})
 
 
-	def denormalizeData(self, t0, t1):
-			minM = self.dataMileages.min()
-			maxM = self.dataMileages.max()
-			minP = self.dataPrices.min()
-			maxP = self.dataPrices.max()
-			return t0 * (maxP - minP) + minP + (t1 * minM * (minP - maxP)) / (maxM - minM), t1 * (maxP - minP) / (maxM - minM)
+	def denormalizeData(self):
+		minM = self.dataMileages.min()
+		maxM = self.dataMileages.max()
+		minP = self.dataPrices.min()
+		maxP = self.dataPrices.max()
+		return self.theta0 * (maxP - minP) + minP + (self.theta1 * minM * (minP - maxP)) / (maxM - minM), self.theta1 * (maxP - minP) / (maxM - minM)
 			
-
 
 	def estimatePrice(self, theta0, theta1, mileage) -> float:
 		try:
@@ -56,16 +56,7 @@ class LinearRegression:
 		plt.ylabel("Prices")
 		plt.title("Plot")
 		i=0
-
 		m = len(self.data)
-
-		price0 = self.estimatePrice(self.theta0, self.theta1, 0)
-		price1 = self.estimatePrice(self.theta0, self.theta1, 1)
-		lin_reg = plt.plot([0, 1], [price0, price1], color='red')
-		plt.ion()
-
-		figure, ax = plt.subplots(figsize=(10, 8))
-		line1, = ax.plot(price0, price1)
 
 		while(True):
 			tmpTheta0 = self.theta0
@@ -80,35 +71,25 @@ class LinearRegression:
 			if (tmpTheta0 == self.theta0 and tmpTheta1 == self.theta1):
 				break
 			i+=1
-			price0 = self.estimatePrice(self.theta0, self.theta1, 0)
-			price1 = self.estimatePrice(self.theta0, self.theta1, 1)
-			line1.set_xdata([price0])
-			line1.set_ydata([price1])
-
-			figure.canvas.draw()
-
-			figure.canvas.flush_events()
- 
-			# time.sleep(0.1)
-    		# plt.pause(1e-10)
-
-
+		price0 = self.estimatePrice(self.theta0, self.theta1, 0)
+		price1 = self.estimatePrice(self.theta0, self.theta1, 1)
+		self.theta0, self.theta1 = self.denormalizeData()
+		print(self.estimatePrice(self.theta0, self.theta1, self.mileage))
+		plt.plot([0, 1], [price0, price1], color='red')
+		plt.show()
 		print(self.theta0, self.theta1)
 		
-
-
-
 
 	def linearRegression(self):
 		mileage = input("Mileage: ")
 		try:
-			mileage = float(mileage)
+			self.mileage = float(mileage)
 		except ValueError:
 			print("Type Error")
 			exit(1)
 		theta = [self.data['km'][0], self.data['price'][0]]
-		price = theta[0] + (theta[1] * mileage)
-		print(f"estimatePrice({int(mileage)}) = {theta[0]:.2f} + ({theta[1]:.2f} ∗ {mileage}) = {price:.2f}")
+		price = theta[0] + (theta[1] * self.mileage)
+		print(f"estimatePrice({int(self.mileage)}) = {theta[0]:.2f} + ({theta[1]:.2f} ∗ {self.mileage}) = {price:.2f}")
 		# plt.show()
 		return price
 
@@ -116,4 +97,3 @@ if __name__ == "__main__":
 	linearRegression = LinearRegression()
 	linearRegression.linearRegression()
 	linearRegression.gradientDescent(0.1)
-
